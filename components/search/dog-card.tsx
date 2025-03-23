98de34cc9218aea756b019de24502401aeb61cd5
Brendan Chess <66980517+brendan-chess@users.noreply.github.com>
2025-03-23 13:04:30 -0500

    select favorite dogs
    added favorites list to zustand store, as well as functions for adding to and removing from favorites. added a heart button to the dog card, which adds/removes a dog from favorites and renders conditionally to show if a dog is a favorite or not

--- components/search/dog-card.tsx
+++ components/search/dog-card.tsx
@@ -8,13 +8,19 @@ import {
   CardDescription,
   CardFooter,
 } from "../ui/card";
+import { Button } from "../ui/button";
+import useStore from "@/lib/store";
+import { Heart } from "lucide-react";
 
 export default function DogCard({ dog }: { dog: Dog }) {
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>{dog.name}</CardTitle>
-        <CardDescription>{dog.breed}</CardDescription>
+      <CardHeader className="flex justify-between">
+        <div className="space-y-2">
+          <CardTitle>{dog.name}</CardTitle>
+          <CardDescription>{dog.breed}</CardDescription>
+        </div>
+        <FavoriteButton dog={dog} />
       </CardHeader>
       <CardContent>
         <Image src={dog.img} alt={dog.name} width={200} height={200} />
@@ -26,5 +32,22 @@ export default function DogCard({ dog }: { dog: Dog }) {
         <p>{dog.zip_code}</p>
       </CardFooter>
     </Card>
+  );
+}
+
+function FavoriteButton({ dog }: { dog: Dog }) {
+  const addFavorite = useStore((state) => state.addFavorite);
+  const removeFavorite = useStore((state) => state.removeFavorite);
+  const favorites = useStore((state) => state.favorites);
+
+  const isFavorite = favorites.some((favorite) => favorite.id === dog.id);
+
+  const handleClick = () =>
+    isFavorite ? removeFavorite(dog) : addFavorite(dog);
+
+  return (
+    <Button onClick={handleClick} variant={"ghost"}>
+      <Heart className={`${isFavorite && "fill-red-500 stroke-red-500"}`} />
+    </Button>
   );
 }
--- lib/store.ts
+++ lib/store.ts
@@ -5,9 +5,12 @@ interface DogState {
   dogs: Dog[];
   next: string | undefined;
   prev: string | undefined;
+  favorites: Dog[];
   setDogs: (dogs: Dog[]) => void;
   setNext: (next: string) => void;
   setPrev: (prev: string) => void;
+  addFavorite: (dog: Dog) => void;
+  removeFavorite: (dog: Dog) => void;
   fetchDogs: (query?: string) => void;
 }
 
@@ -15,9 +18,16 @@ const useStore = create<DogState>()((set) => ({
   dogs: [],
   next: undefined,
   prev: undefined,
+  favorites: [],
   setDogs: (dogs: Dog[]) => set({ dogs }),
   setNext: (next: string) => set({ next }),
   setPrev: (prev: string) => set({ prev }),
+  addFavorite: (dog: Dog) =>
+    set((state) => ({ favorites: [...state.favorites, dog] })),
+  removeFavorite: (dog: Dog) =>
+    set((state) => ({
+      favorites: state.favorites.filter((d) => d.id !== dog.id),
+    })),
   fetchDogs: async (query = "/dogs/search") => {
     // Fetch dogs from the search route
     const response = await fetch(
